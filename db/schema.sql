798116a3a739c1594ba1c7a2180f16f2d3336292
Derek Phelps <derekphtpt@gmail.com>
2021-07-28 11:53:24 -0600

    foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE employee_role(
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,2) NOT NULL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -20,5 +21,7 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT 
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES employee_role(id) 
 );
